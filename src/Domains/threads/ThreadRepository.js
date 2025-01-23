1c8d386be76330dad3925b980e2b439863df975d
Daniel Sidauruk <macbook@Daniel.local>
2025-01-23 18:40:05 +0700

    feat: add ThreadRepository

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -1,5 +1,21 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
 class ThreadRepository {
+  async addThread(thread) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 
+  async getThreadById(id) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyThreadIsExistById(id) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteThreadById(id) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ThreadRepository;
