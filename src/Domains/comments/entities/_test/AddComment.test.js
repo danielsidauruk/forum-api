0c31d4365c9e7343d04a30da8a3043f8a12cc66e
Daniel Sidauruk <macbook@Daniel.local>
2025-01-24 11:40:35 +0700

    feat: add AddComment entities

---
+++ src/Domains/comments/entities/_test/AddComment.test.js
@@ -0,0 +1,42 @@
+const AddComment = require('../AddComment');
+
+describe('a AddComment entities', () => {
+  it('should throw error when payload did not contain needed properties', () => {
+    // Arrange
+    const payload = {
+      content: 'Dicoding Indonesia is the best',
+    };
+
+    // Action & Assert
+    expect(() => new AddComment(payload)).toThrowError('ADD_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      content: 'Dicoding Indonesia is the best',
+      owner: true,
+      threadId: {},
+    };
+
+    // Action & Assert
+    expect(() => new AddComment(payload)).toThrowError('ADD_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATIONS');
+  });
+
+  it('should create AddComment object correctly', () => {
+    // Arrange
+    const payload = {
+      content: 'Dicoding Indonesia is the best',
+      owner: 'johndoe',
+      threadId: 'thread-123',
+    };
+
+    // Action
+    const { content, owner, threadId } = new AddComment(payload);
+
+    // Assert
+    expect(content).toEqual(payload.content);
+    expect(owner).toEqual(payload.owner);
+    expect(threadId).toEqual(payload.threadId);
+  });
+});
